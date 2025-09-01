3b4d1f72d66a6e39a4410e2954549ed4d25e8f93
FSD9-Wave <loomaflex44@gmail.com>
2025-09-01 15:36:59 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-3xl font-bold underline justify-center items-center">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
