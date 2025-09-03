273d8ac53138cc425ab4ae0d53059cc3787affaa
FSD9-Wave <loomaflex44@gmail.com>
2025-09-03 15:34:40 +0700

    feat: add HeroSection component and enhance layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './index.css'
 import Navbar from './components/navbar'
+import HeroSection from './components/herosection'
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col items-center w-full">
       <Navbar/>
+      <HeroSection/>
     </div>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center w-[100vw] h-[80px] bg-[#F9F8F6] border-b pt-4 pr-[120px] pb-4 pl-[120px]">
+    <div className="flex justify-between items-center w-[100vw] h-[80px] bg-[#F9F8F6] border-b border-[#DAD6D1] pt-4 pr-[120px] pb-4 pl-[120px]">
         <div className="flex gap-6">
             <h1 className="text-[#26231E] text-3xl font-poppins font-bold">Wave-MaShare <span className="text-[#128279]">.</span></h1>
         </div>
